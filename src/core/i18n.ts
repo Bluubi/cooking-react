5fc50d9a66c28e01a42698857f900c8c12af3f48
BluubiDev <bluubi.dev@gmail.com>
2024-03-15 20:00:14 +0100

    add translation

---
+++ src/core/i18n.ts
@@ -0,0 +1,35 @@
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .use(LanguageDetector)
+    .init({
+        // the translations
+        // (tip move them in a JSON file and import them,
+        // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+        resources: {
+            en: {
+                translation: {
+                    home: {
+                        "welcomeToCooking": "Welcome to <strong>Cooking React</strong>",
+                        "guest": "Log as guest",
+                    }
+                }
+            },
+            es: {
+                translation: {
+                    home: {
+                        "welcomeToCooking": "Bienvenido a <strong className='text-'>Cooking React</strong>",
+                        "guest": "Entrar como invitado",
+                    }
+                }
+            },
+        },
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+        }
+    });
+
+export default i18n;
