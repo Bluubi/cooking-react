07f7c753de88301f5ea3aca114f1ba9fdb5ed348
BluubiDev <bluubi.dev@gmail.com>
2024-03-22 19:23:45 +0100

    comment cookie

--- src/features/meal/application/index.tsx
+++ src/features/meal/application/index.tsx
@@ -4,7 +4,6 @@ import {setCookie} from "../../../utils/cookies/set.ts";
 import {getCookie} from "../../../utils/cookies/get.ts";
 import {MealRecipe} from "../domain/Meal.ts";
 import {Card} from "../../../components/card";
-import brushSVG from '../../../assets/brush.svg';
 
 interface MealProps {
     meals: Meals | undefined
@@ -17,7 +16,7 @@ export const Meal = ({ meals }: MealProps) => {
     const cookie = getCookie('recipeForDay');
     console.log(cookie);
     if(cookie !== null) {
-        setMeal(JSON.parse(cookie));
+        //setMeal(JSON.parse(cookie));
         return null;
     }
         if(meals){
