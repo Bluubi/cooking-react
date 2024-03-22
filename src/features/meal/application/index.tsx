65e84bc2d6ea2ca79144180360a2a2df48ed850d
BluubiDev <bluubi.dev@gmail.com>
2024-03-22 19:53:07 +0100

    fix possible error when trying to store recipe in cookie

--- src/features/meal/application/index.tsx
+++ src/features/meal/application/index.tsx
@@ -1,13 +1,9 @@
-import {Meals} from "../../meals/domain/Meals.ts";
 import {useEffect, useState} from "react";
 import {setCookie} from "../../../utils/cookies/set.ts";
 import {getCookie} from "../../../utils/cookies/get.ts";
-import {MealRecipe} from "../domain/Meal.ts";
+import {getRandomMeal, MealProps, MealRecipe} from "../domain/Meal.ts";
 import {SpecialCard} from "../../../components/card/special";
-
-interface MealProps {
-    meals: Meals | undefined
-}
+import {Meals} from "../../meals/domain/Meals.ts";
 
 export const Meal = ({ meals }: MealProps) => {
 
@@ -15,24 +11,31 @@ export const Meal = ({ meals }: MealProps) => {
     const setRecipeForDay = () => {
     const cookie = getCookie('recipeForDay');
     if(cookie !== null) {
-        // setMeal(JSON.parse(cookie));
+        try {
+            setMeal(JSON.parse(cookie));
+        }catch (error){
+            setAndGetRandomMeal(meals);
+            console.error('Something wrong happened')
+        }
         return null;
     }
+
+    }
+
+    function setAndGetRandomMeal(meals: Meals | undefined): MealRecipe | undefined {
+        const randomMeal = getRandomMeal(meals);
+        setMeal(randomMeal);
         if(meals){
-            const randomNumber = Math.floor(Math.random() * meals?.meals.length);
-            const randomMeal = meals.meals[randomNumber];
-            setMeal({
-                idMeal: meals.meals[randomNumber].idMeal!,
-                strMealThumb: meals.meals[randomNumber].strMealThumb!,
-                strMeal: meals.meals[randomNumber].strMeal!
-            });
-            setCookie<any>('recipeForDay', 1, randomMeal);
+            if(randomMeal){
+                setCookie<MealRecipe>('recipeForDay', 1, randomMeal);
+            }
         }
+        return randomMeal;
     }
 
     useEffect(() => {
         setRecipeForDay();
-    }, []);
+    }, [])
 
     return (<article >
         <SpecialCard id={meal?.idMeal} title={meal?.strMeal} image={meal?.strMealThumb}/>
--- src/features/meal/domain/Meal.ts
+++ src/features/meal/domain/Meal.ts
@@ -1,5 +1,24 @@
+import {Meals} from "../../meals/domain/Meals.ts";
+
 export interface MealRecipe {
     idMeal: string;
     strMeal: string;
     strMealThumb: string;
+}
+
+export interface MealProps {
+    meals: Meals | undefined
+}
+
+
+export function getRandomMeal (meals: Meals | undefined): MealRecipe | undefined {
+    if(meals){
+        const randomNumber = Math.floor(Math.random() * meals?.meals.length);
+        return {
+            idMeal: meals.meals[randomNumber].idMeal!,
+            strMealThumb: meals.meals[randomNumber].strMealThumb!,
+            strMeal: meals.meals[randomNumber].strMeal!
+        };
+    }
+    return undefined;
 }
