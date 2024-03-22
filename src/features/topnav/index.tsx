6d193a86fd69c4b6f105196875530d0592239895
BluubiDev <bluubi.dev@gmail.com>
2024-03-22 23:10:33 +0100

    fix: assign key to topnav elements

--- src/features/topnav/index.tsx
+++ src/features/topnav/index.tsx
@@ -7,7 +7,7 @@ import {Trans} from "react-i18next";
 export const Topnav = () => {
     return ( <nav className={'relative flex bg-emerald-950 text-white'}>
         <ul className={'flex gap-x-3.5'}>
-            { navOptions.map( option => <li className={'p-3.5 hover:bg-emerald-600 cursor-pointer'}>{ option }</li>)}
+            { navOptions.map( (option, idx) => <li key={`idx-${idx}`} className={'p-3.5 hover:bg-emerald-600 cursor-pointer'}>{ option }</li>)}
         </ul>
     </nav>)
 }
