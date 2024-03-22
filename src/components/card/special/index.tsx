4f8d9d1c0a8db9fa35e0519fc7f20f4bfa61f24d
BluubiDev <bluubi.dev@gmail.com>
2024-03-22 20:27:00 +0100

    change style

--- src/components/card/special/index.tsx
+++ src/components/card/special/index.tsx
@@ -6,7 +6,6 @@ export const SpecialCard = ({id, title, image }: SpecialCardProps) => {
         <div key={id} className={'w-svw h-72 flex shadow-md'}>
             <div className={'h-full w-full flex flex-wrap'}>
                 <p className={'text-2xl self-end p-3.5'}> Hoy te recomendamos... </p>
-                <h2 className={'font-bold text-xl p-3.5'}>{title}</h2>
             </div>
             {image && title && <img
                 className={'w-full h-full object-cover transform transition-all duration-500 hover:scale-110 hover:cursor-pointer'}
